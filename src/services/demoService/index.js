cb50c374a895577eaab78f8381abdbe7db041fc3
jhonquiceno <jonasquiceno@gmail.com>
2023-08-23 18:18:53 -0500

    Add files via upload

---
+++ src/services/demoService/index.js
@@ -0,0 +1,13 @@
+// aca va el servicio
+
+class DemoService {
+    constructor() {
+        this.url = 'http://miurl'
+    }
+
+    getStudents () {
+        return ['juan', 'pedro'];
+    }
+}
+
+export default DemoService;
